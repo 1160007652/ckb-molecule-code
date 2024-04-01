e229d51f27d80dc9aee297f23ca258aaa7d65d75
muniz <1160007652@qq.com>
2024-04-01 17:32:41 +0800

    fix: Object replace Map

--- src/table/encode.ts
+++ src/table/encode.ts
@@ -1,6 +1,6 @@
 // table MixedType { f1: Bytes, f2: byte, f3: Uint32, f4: Byte3, f5: Bytes }
 
-import type { HexType, IMolecule, IMoleculeType } from "../types";
+import type { HexType, IMolecule } from "../types";
 import {
   bytesToHex,
   hexToBytes,
@@ -12,43 +12,36 @@ import {
 // uint32 低位存储, 00 00 00 00
 const offsetSize = 4;
 
-export default function encode<T extends IMolecule>(
-  molecule: T,
-  moleculeType: IMoleculeType<T>
-): HexType {
-  // 提取编码元素的key键, 用于映射
-  const moleculeKeys = Object.keys(molecule);
-
+export default function encode(molecule: IMolecule): HexType {
   // FullSize + offsetSize => headerSize
-  const headerSize = (moleculeKeys.length + 1) * offsetSize;
+  const headerSize = (molecule.size + 1) * offsetSize;
 
   // 存储每一个 molecule 转换后的编码
   const bodys: string[] = [];
 
   let headerOffSetStep = headerSize;
   let offsetHex = toUint32Le(`0x${headerSize.toString(16)}`).slice(2);
+  let index = 0;
 
   // 序列化 不同类型的 molecule 元素
-  moleculeKeys.forEach((key, index) => {
-    const itemType = moleculeType[key];
-    const itemvalue = molecule[key];
-
-    if (itemType == "Uint32") {
-      bodys.push(toUint32Le(serializeArray(itemvalue)).slice(2));
+  molecule.forEach(([hexValue, utilType], objKey) => {
+    if (utilType == "Uint32") {
+      bodys.push(toUint32Le(serializeArray(hexValue)).slice(2));
     }
-    if (itemType === "byte") {
-      bodys.push(serializeArray(itemvalue).slice(2));
+    if (utilType === "byte") {
+      bodys.push(serializeArray(hexValue).slice(2));
     }
-    if (/^Byte\d+/g.test(itemType)) {
-      bodys.push(serializeArray(itemvalue).slice(2));
+    if (/^Byte\d+/g.test(utilType)) {
+      bodys.push(serializeArray(hexValue).slice(2));
     }
-    if (itemType === "Bytes") {
-      bodys.push(serializeBytes(itemvalue).slice(2));
+    if (utilType === "Bytes") {
+      bodys.push(serializeBytes(hexValue).slice(2));
     }
 
-    if (index < moleculeKeys.length - 1) {
+    if (index < molecule.size - 1) {
       headerOffSetStep += bodys[index].length / 2;
       offsetHex += toUint32Le(`0x${headerOffSetStep.toString(16)}`).slice(2);
+      index++;
     }
   });
 
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,8 +2,4 @@ export type HexType = `0x${string}`;
 
 export type UtilType = "Bytes" | "byte" | "Uint32" | `Byte${number}`;
 
-export type IMolecule = Record<string, HexType | Uint8Array>;
-
-export type IMoleculeType<T> = {
-  [K in keyof T]: UtilType;
-};
+export type IMolecule = Map<string, [HexType | Uint8Array, UtilType]>;
